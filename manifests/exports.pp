7d55100239b7324606360bafc6fb9bf052ff45a2
Niklaus Giger <niklaus.giger@member.fsf.org>
2014-07-19 21:33:19 +0200

    Added new class nfs::exports which is able to get the definitions via hiera

---
+++ manifests/exports.pp
@@ -0,0 +1,28 @@
+define one_export( 
+  $path = '',
+  $options = [],
+  $clients = [],
+        ) {
+  
+  if (!defined(File["$path"])) {
+    file{"$path": ensure => directory}
+  }
+
+  nfs::export { $path:
+      options => $options,
+      clients => $clients,
+      require => File[$path],
+  }  
+}
+
+class nfs::exports ( $definitions = {}) {
+
+  if ($puppetversion =~ /^[12]/) {
+    notify{"Your puppet version $puppetversion is too old to use nfs::exports. Required is puppet >= 3.0": }
+  }
+  else {
+    include nfs::server
+    create_resources(one_export, $definitions, {})
+  } 
+}
+

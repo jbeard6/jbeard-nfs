1b3b8e3f5f9fc314d6c907bf179cd7278a8c6d47
Colin Wood <cwood06@gmail.com>
2015-03-25 13:48:07 -0700

    Change service names for centos 7

--- manifests/server/rhel/services.pp
+++ manifests/server/rhel/services.pp
@@ -5,14 +5,25 @@ class nfs::server::rhel::services (
 
     require portmap
 
-    service { 'nfs':
+    case $::operatingsystemmajrelease {
+      "7": {
+        $nfs_service = 'nfs-server'
+        $nfs_lock_service = 'nfs-lock'
+      }
+      default: {
+        $nfs_service = 'nfs'
+        $nfs_lock_service = 'nfslock'
+      }
+    }
+
+    service { $nfs_service:
         ensure     => $ensure,
         enable     => $enable,
         hasstatus  => true,
         hasrestart => true,
     }
 
-    service { 'nfslock':
+    service { $nfs_lock_service:
         ensure     => $ensure,
         enable     => $enable,
         hasstatus  => true,
